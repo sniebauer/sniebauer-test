af813bed3b385b447c8a4e097bcba9c6c7dceb1b
sniebauer <steve@vhx.tv>
2017-08-28 14:31:48 -0500

    update

--- src/Web.js
+++ src/Web.js
@@ -13,9 +13,9 @@ class Web extends Component {
         <ul className="Web-ul">
           <li className="Web-li"><a href="https://vimeo.com/blog/author/sniebauer" target="new">Vimeo Blog Postings</a> - Various blog posts and lessons written by me for the official Vimeo blog.</li>
 
-          <li className="Web-li"><a href="http://help.vhx.tv/" target="new">VimeoVHX Seller Support</a> - asdfasdfasdfasfasdasfdasfasdfasdf</li>
+          <li className="Web-li"><a href="http://help.vhx.tv/" target="new">VimeoVHX Seller Support</a> - The official help center for VimeoVHX sellers - run on Help Scout & written by me.</li>
 
-          <li className="Web-li"><a href="http://support.vhx.tv/" target="new">VimeoVHX Customer Support</a> - asdfasdfasdfasfasdasfdasfasdfasdf</li>
+          <li className="Web-li"><a href="http://support.vhx.tv/" target="new">VimeoVHX Customer Support</a> - The official help center for VimeoVHX customers - run on Help Scout & written by me.</li>
 
           <li className="Web-li"><a href="http://heydoggies.tumblr.com/" target="new">Hey Doggies!</a> - Tumblr site with curated photos and video of cute dogs, etc.</li>
 
