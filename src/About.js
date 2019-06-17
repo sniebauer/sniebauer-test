5ab5319b75400ad91f4728b0e22526229882d1bb
Steve Niebauer <sniebauer@gmail.com>
2019-06-17 16:43:49 -0500

    update about.js

--- src/About.js
+++ src/About.js
@@ -14,21 +14,19 @@ class About extends Component {
           <img src={me} alt='Steve Niebauer' />
           </Col>
 	       <Col s={12} l={6} m={6} className='About-p'>
-          <p>Hi there! My name is Steve & I'm a support manager in the video-tech industry. I got my start right out of school at a little video site called <a href="http://vimeo.com/" target="new">Vimeo</a>. I then moved on to help start & scale the product & support department at video startup, <a href="https://vimeo.com/226501794" target="new">VHX</a>, which <a href="https://techcrunch.com/2016/05/02/vimeo-acquires-vhx-to-boost-its-video-on-demand-business/" target="new">got aquired by Vimeo in 2016</a>, where I am currently employed (again). I reside in Chicago with my wife, active son, <a href="#top">Felix</a>, and dog, <a href="https://vimeo.com/35639629" target="new">Lucy</a>.</p>
+          <p>Hi there! My name is Steve & I'm a support specialist in the video-tech industry. I got my start right out of school at a little video site called <a href="http://vimeo.com/" target="new">Vimeo</a>. I then moved on to help start & scale the product & support department at video startup, <a href="https://vimeo.com/226501794" target="new">VHX</a>, which <a href="https://techcrunch.com/2016/05/02/vimeo-acquires-vhx-to-boost-its-video-on-demand-business/" target="new">got aquired by Vimeo in 2016</a>. I then spent 2 years building out the OTT support team there. I am now at Bitmovin, helping shape the future of video online. I reside in Chicago with my wife, 2 boys, and dog, <a href="https://vimeo.com/35639629" target="new">Lucy</a>.</p>
 
           <p>Here are some things I know & like:</p>
 
           <ul className='About-ul'>
-            <li className='About-li'>Support operations/community management</li>
-            <li className='About-li'>HTML/CSS/Javascript</li>
-            <li className='About-li'>React JS</li>
-            <li className='About-li'>Video editing with Adobe & Apple products</li>
-            <li className='About-li'>Video production</li>
-            <li className='About-li'>Photography</li>
-            <li className='About-li'>Video games</li>
+            <li className='About-li'>Support operations & management</li>
+            <li className='About-li'>HTML/CSS/Javascript/React</li>
+            <li className='About-li'>FOOD</li>
+            <li className='About-li'>Video production & editing</li>
+            <li className='About-li'>Indie games</li>
           </ul>
 
-          <p>Check out my <a href="https://www.linkedin.com/in/sniebauer/" target="new">LinkedIn</a> & <a href="https://github.com/sniebauer" target="new">GitHub</a> pages for more.</p>
+          <p>Check out my <a href="https://www.linkedin.com/in/sniebauer/" target="new">LinkedIn</a> for more. Thanks!</p>
 
           </Col>
         </Row>
